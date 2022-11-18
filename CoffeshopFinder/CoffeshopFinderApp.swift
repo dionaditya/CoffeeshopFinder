efa37bb46a3c75d1fcd125bc1cdf593ad24f0237
Dion Aditya <dion.aditya@kiteworks.com>
2022-11-18 20:03:41 +0700

    Initial Commit

---
+++ CoffeshopFinder/CoffeshopFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeshopFinderApp.swift
+//  CoffeshopFinder
+//
+//  Created by Dion Aditya on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeshopFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
